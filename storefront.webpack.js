7ada586ea92fa55af1714fe1e47c856976af488c
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-08-01 23:15:51 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,8 +3,8 @@ console.log(path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'
 module.exports = () => ({
   resolve: {
     alias: {
-      //'./js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
-      //'./html/ShippingLine.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingLine.html')
+      './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js'),
+      './html/ShippingLine.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingLine.html')
     }
   }
 })
